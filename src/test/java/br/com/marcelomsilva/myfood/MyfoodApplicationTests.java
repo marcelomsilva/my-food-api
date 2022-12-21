d1925e2adfadfd7b8b3b23c997adbe6dddb55c72
marcelomsilva <marcelo96comercial@gmail.com>
2022-12-20 21:37:56 -0300

    [add] initial files

---
+++ src/test/java/br/com/marcelomsilva/myfood/MyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelomsilva.myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
